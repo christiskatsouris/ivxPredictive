58fbb09f2ef0b1b529c43647a7b935ee72248635
Dr Christis Katsouris <43221582+christiskatsouris@users.noreply.github.com>
2022-11-30 01:20:13 +0200

    Update IVXP.R

--- R/IVXP.R
+++ R/IVXP.R
@@ -1,1 +1,14 @@
+###########################################
+# R Script Details:
+###########################################
 
+# Script name: IVXP.R
+
+# Program aim: This R program implements the Wald IVXP test in linear predictive regression models.  
+
+# written by: 
+
+# Christis G. Katsouris (August 2022)
+# Department of Economics
+# University of Southampton
+# Southampton, United Kingdom
